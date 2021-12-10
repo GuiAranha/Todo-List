0a473ae6e8d59553d756844a9c93d2d222b9b313
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2021-12-10 17:16:58 -0300

    Requisito 7 e 8 -ok

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 window.onload = function () {
     let botaoCriar = document.getElementById('criar-tarefa');
+    let lista = document.getElementById('lista-tarefas');
 
     botaoCriar.addEventListener('click', function(event){
-        let lista = document.getElementById('lista-tarefas');
         let conteudo = document.getElementById('texto-tarefa');
         let item = document.createElement('li');
         item.innerText = conteudo.value;
         lista.appendChild(item);
         conteudo.value = "";
-    })
+    });
+
+    lista.addEventListener('click', function(event){
+
+        for ( let i = lista.firstElementChild; i !== null; i = i.nextElementSibling){
+            if(i.style.background = "rgb(128, 128, 128)"){
+                i.style.background = "white";
+            }
+        }
+        event.target.style.background = "rgb(128, 128, 128)";
+    });
 };
